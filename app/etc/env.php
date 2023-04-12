27453a1a543a7d1e88a4cbd981d7f9b6e45119a0
AdamHusssain <emb-adamhus@embitel.com>
2023-04-12 18:21:16 +0530

    initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'e57df5ab84bb8359ffd0c4fa0e8fde3d'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f73_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f73_'
+            ]
+        ],
+        'allow_parallel_generation' => false,
+        'graphql' => [
+            'id_salt' => '9dJqeD1r8wsro2vtRguQmHNIgc8fDKwG'
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Tue, 04 Apr 2023 11:29:50 +0000'
+    ]
+];
